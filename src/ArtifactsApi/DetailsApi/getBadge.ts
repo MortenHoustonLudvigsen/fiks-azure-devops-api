a86de5836cea4db3d0fb7df0df9195a52eb4655a
Morten Houston Ludvigsen <ttmlu000@foa.dk>
2025-04-27 14:45:23 +0200

    Implement getBadge

---
+++ src/ArtifactsApi/DetailsApi/getBadge.ts
@@ -0,0 +1,26 @@
+import { DetailsApi } from './DetailsApi';
+
+declare module './DetailsApi' {
+  interface DetailsApi {
+    /**
+     * Generate a SVG badge for the latest version of a package.
+     *
+     * The generated SVG is typically used as the image in an HTML link which takes users to the feed containing the package to accelerate discovery and consumption.
+     * 
+     * The project parameter must be supplied if the feed was created in a project. If the feed is not associated with any project, omit the project parameter from the request.
+     * @param project Project ID or project name
+     * @param feedId Name or Id of the feed.
+     * @param packageId Id of the package (GUID Id, not name).
+     * @param options Options
+     */
+    getBadge(project: string | undefined, feedId: string, packageId: string): Promise<Buffer>;
+  }
+}
+
+Object.assign(DetailsApi.prototype, {
+  async getBadge(this: DetailsApi, project: string | undefined, feedId: string, packageId: string): Promise<Buffer> {
+    return await this.getBinary([project, '_apis', 'public', 'packaging', 'Feeds', feedId, 'Packages', packageId, 'badge'], {
+      'api-version': '7.2-preview.1',
+    });
+  },
+});
